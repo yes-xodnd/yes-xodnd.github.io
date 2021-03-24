08b60f1c5f3a07c3665048173cdab5a019811dea
yes-xodnd <taew3167@gmail.com>
2021-03-25 01:27:03 +0900

    Deploy gh pages

---
+++ out/_next/static/eK4oJGUCOHLzLQUMl40nI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e89ab44a61e2d933b0f9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9f646e9e83eb4d7da6d9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
