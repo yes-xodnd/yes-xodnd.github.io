220b91e93da0363ab1086c85a1046682f3865da8
Taewoong Park <78471588+yes-xodnd@users.noreply.github.com>
2021-03-27 05:20:54 +0900

    Revert "Add feature: markdown parser (#1)" (#2)
    This reverts commit db6f55b3535fe61a32fb4879dba856349d11fecc.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import { getMatteredPosts } from '../lib/posts';
+
 function Blog({ allPosts }) {
+
+  const PostItem = ({ title, id }) => (
+    <li key={ id }>
+      <h3>{ title }</h3>
+    </li>
+  )
+
+  const PostList = ({ posts }) => (
+    <ul>
+      { posts.map(post => PostItem(post)) }
+    </ul>
+  )
 
   return (
     <div>
       <h1>yes-xodnd blog</h1>
+
+      <PostList posts={allPosts} />
     </div>
   )
 }
 
 export default Blog
+
+
+export async function getStaticProps() {
+  const allPosts = getMatteredPosts()
+  return {
+    props: {
+      allPosts
+    }
+  }
+} 
