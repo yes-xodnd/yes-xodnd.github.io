4d10ab8ebe0210e45384881e858c533fe4d95bc7
yes-xodnd <taew3167@gmail.com>
2021-03-27 05:06:58 +0900

    Delete unrelated files

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
-import { getAllPosts } from '../lib/api'
-import PostList from '../components/PostList'
-
 function Blog({ allPosts }) {
 
   return (
     <div>
       <h1>yes-xodnd blog</h1>
-
-      <h2>게시물 목록</h2>
-      <PostList posts={allPosts} />
     </div>
   )
 }
 
 export default Blog
-
-
-export async function getStaticProps() {
-  const allPosts = getAllPosts(['slug', 'title', 'date'])
-  return {
-    props: {
-      allPosts
-    }
-  }
-} 
