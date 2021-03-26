2ee28bedf8ea5c3966597bae1804d85bd7813f88
yes-xodnd <taew3167@gmail.com>
2021-03-27 05:28:07 +0900

    Add PostList

---
+++ components/PostList.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+const PostItem = ({ slug, title, date }) => (
+  <li key={ slug }>
+      <h3>{ title }</h3>
+      <div>
+        <span>{ date }</span>
+      </div>
+  </li>
+)
+
+const PostList = ({ posts }) => (
+  <ul>
+    { posts.map(post => PostItem(post)) }
+  </ul>
+)
+
+export default PostList
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
+// lib
+import { getAllPosts } from '../lib/api'
+
+// component
+import PostList from '../components/PostList'
+
 function Blog({ allPosts }) {
 
   return (
     <div>
       <h1>yes-xodnd blog</h1>
+
+      <h2>게시물 목록</h2>
+      <PostList posts={allPosts} />
     </div>
   )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts(['slug', 'title', 'date'])
+  return {
+    props: {
+      allPosts
+    }
+  }
+} 
