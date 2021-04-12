dcfc69869ff574b40b0fa6106b75aa90f81bc1a5
yes-xodnd <taew3167@gmail.com>
2021-04-12 18:16:14 +0900

    Separate GlobalStyle and theme

---
+++ style/theme.js
@@ -0,0 +1,13 @@
+export default {
+  color: {
+    green: 'seagreen',
+    blue: 'royalblue',
+    lightgrey: 'rgb(245, 245, 245)',
+    darkgrey: 'rgb(50, 50, 50)',
+  },
+  font: {
+    sm: '0.8rem',
+    md: '1rem',
+    lg: '2rem',
+  }
+}
