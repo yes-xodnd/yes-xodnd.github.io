44ff2feb6021a95c2794498d1cf6abbea306ee39
yes-xodnd <taew3167@gmail.com>
2021-04-18 17:58:42 +0900

    Fix Main margin

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import Header from './Header'
-import SideBar from './Sidebar'
 import Footer from './Footer'
 import ButtonScrollUp from './ButtonScrollUp'
 
@@ -20,5 +19,5 @@ export default function Layout({ Component, pageProps }) {
 
 const Main = styled.main`
   max-width: 1024px;
-  margin: 50px auto 100px;
+  margin: 3rem auto 10rem;
 `
