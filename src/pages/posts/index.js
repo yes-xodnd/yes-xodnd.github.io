5f656941cb2aeb381d58ba4dfef398248ecc432e
yes-xodnd <taew3167@gmail.com>
2021-05-08 23:56:28 +0900

    Update allTags: count

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -3,10 +3,13 @@ import { useState } from 'react'
 import { getAllPosts } from '../../lib/api'
 import PostListItem from '../../components/PostListItem'
 import TagList from '../../components/TagList'
+import ButtonScrollUp from '../../components/ButtonScrollUp'
+import ButtonToggleTagList from '../../components/ButtonToggleTagList'
 
 function Posts({ allPosts, allTags }) {
-  const [posts, setPosts] = useState(allPosts)
+  const [ posts, setPosts ] = useState(allPosts)
   const [ selectedTag, setSelectedTag ] = useState('')
+  const [ isVisibleTagList, setVisibleTagList ] = useState(false)
 
   const handleClickTag = e => {
     const tag = e.target.innerText
@@ -19,9 +22,16 @@ function Posts({ allPosts, allTags }) {
     )
   }
 
+  const toggleVisibleTagList = () => setVisibleTagList(!isVisibleTagList)
+
   return (
     <Container>
-      <TagList { ...{ allTags, selectedTag, handleClickTag } } />
+      <TagList { ...{ 
+        allTags,
+        selectedTag,
+        handleClickTag,
+        isVisible: isVisibleTagList,
+      } } />
       <ListContainer>
         <List>
           { posts.map(post => 
@@ -33,6 +43,9 @@ function Posts({ allPosts, allTags }) {
           }
         </List>
       </ListContainer>
+
+      <ButtonToggleTagList handleClick={toggleVisibleTagList} />
+      <ButtonScrollUp />
     </Container>
   ) 
 }
@@ -59,14 +72,16 @@ function getAllTags(allPosts) {
       map[tag] = map[tag] ? map[tag] + 1 : 1
     }
   }
-  return Object.keys(map).sort((a, b) => map[b] - map[a])
+  return Object.entries(map).map(([tagName, count]) => ({ tagName, count }));
+  // return Object.keys(map).sort((a, b) => map[b] - map[a])
 }
 
 // style
 const Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  margin: 3rem 1rem 0;
+  padding: 3rem 1rem 0;
 
   @media screen and (min-width: 768px) {
     flex-direction: row;
