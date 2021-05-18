bc587abfda538665c63ba9894d3ffd9a62f94aea
yes-xodnd <taew3167@gmail.com>
2021-05-18 19:11:28 +0900

    Update layout

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -6,13 +6,15 @@ import codeHighlight from '../../style/codeHighlight'
 
 export default function Post({ post }) {
   return (
+    <>
     <Container>
-      <Header>
-        <Title>{ post.title }</Title>
-        <Date>{ dateFormat(post.date) }</Date>
-      </Header>
+    <Header>
+      <Title>{ post.title }</Title>
+      <Date>{ dateFormat(post.date) }</Date>
+    </Header>
       <Content dangerouslySetInnerHTML={{ __html: post.content }} />
     </Container>
+    </>
   )
 }
 
@@ -53,18 +55,21 @@ export async function getStaticPaths() {
 
 // style
 const Container = styled.section`
+  min-height: 100vh;
+  padding: 2rem;
   margin: auto;
-  padding: 0 2rem;
   max-width: 768px;
 `
 
 const Header = styled.div`
-  margin: 2rem 0;
+  margin: 2rem 0 5rem;
+  text-align: center;
 `
 
 const Title = styled.h1`
   margin: 0 0 1rem;
   font-size: ${({ theme }) => theme.font.lg };
+  word-break: keep-all;
 `
 
 const Date = styled.div`
