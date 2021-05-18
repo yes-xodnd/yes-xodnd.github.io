676c35d122c598b5a3209b10d3d30ee8cc2eebb2
yes-xodnd <taew3167@gmail.com>
2021-05-18 16:24:57 +0900

    Update getStaticProps: sort tags by post count

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,104 @@
-import Link from 'next/link'
-import { useEffect, useRef } from 'react'
+import styled from 'styled-components'
+import { useState } from 'react'
+import { getAllPosts } from '../lib/api'
 
-function Home() {
-  const linkRef = useRef(null);
+// components
+import Header from '../components/Header'
+import PostListItem from '../components/PostListItem'
+import TagList from '../components/TagList'
+import ButtonScrollUp from '../components/ButtonScrollUp'
 
-  useEffect(() => {
-    linkRef.current.dispatchEvent(new Event('click', { bubbles: true }));
-  })
+function Posts({ allPosts, allTags }) {
+  const [ posts, setPosts ] = useState(allPosts)
+  const [ selectedTag, setSelectedTag ] = useState('')
+  const [ isVisibleTagList, setVisibleTagList ] = useState(false)
+
+  const handleClickTag = e => {
+    const tag = e.target.innerText
+    const newSelectedTag = selectedTag === tag ? '' : tag
+    setSelectedTag(newSelectedTag)
+    setPosts(
+      !newSelectedTag
+      ? allPosts
+      : allPosts.filter(post => post.tags.includes(tag))
+    )
+  }
+
+  const toggleVisibleTagList = () => setVisibleTagList(!isVisibleTagList)
 
   return (
-    <Link href='/posts'>  
-      <div ref={linkRef} ></div>
-    </Link>
-  )
+    <Container>
+    <Header />
+      <TagList { ...{ 
+        allTags,
+        selectedTag,
+        handleClickTag,
+        isVisible: isVisibleTagList,
+      } } />
+      <ListContainer>
+        <List>
+          { posts.map(post => 
+            <PostListItem
+              post={post}
+              handleClickTag={handleClickTag}
+              key={post.slug}
+            />)
+          }
+        </List>
+      </ListContainer>
+
+      <ButtonToggleTagList handleClick={toggleVisibleTagList} />
+      <ButtonScrollUp />
+    </Container>
+  ) 
 }
-export default Home
+export default Posts
+
+// static
+export function getStaticProps() {
+  const allPosts = getAllPosts(['slug', 'title', 'date', 'tags'])
+  const allTags = getAllTags(allPosts)
+  
+  return {
+    props: {
+      allPosts,
+      allTags
+    }
+  }
+} 
+
+function getAllTags(allPosts) {
+  const map = {}
+
+  for (let post of allPosts) {
+    for (let tag of post.tags) {
+      map[tag] = map[tag] ? map[tag] + 1 : 1
+    }
+  }
+  return Object.keys(map).sort((a, b) => map[b] - map[a])
+}
+
+// style
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 3rem 1rem 0;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`
+
+const ListContainer = styled.section`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+`
+
+const List = styled.ul`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+`
