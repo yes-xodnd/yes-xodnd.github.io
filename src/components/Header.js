27d2d3b21af88e5c3813f75cba9af1dd4c28f061
yes-xodnd <taew3167@gmail.com>
2021-05-18 17:53:28 +0900

    Add nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,30 @@ export default function Header() {
   return (
     <Container>
       <Content>
-        <Link href="/">
-          <div>
-            YES-XODND <br />
-            DEV BLOG.
-          </div> 
-        </Link>
+        <Nav>
+          <Link href="/">HOME</Link>
+          <Link href="/posts">POSTS</Link>
+        </Nav>
       </Content>
     </Container>
   )
 }
 
 const Container = styled.header`
+  background: white;
   border-bottom: 1px solid black;
 `
 
 const Content = styled.div`
   display: flex;
-  justify-content: space-between;
-  padding: 2rem;
+  justify-content: flex-end;
+
+  padding: 1rem;
   margin: auto;
   max-width: 1024px;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 1rem;
 `
