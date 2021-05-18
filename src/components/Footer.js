62ab098d01fc166ee6b256db6c99dabff026ada9
yes-xodnd <taew3167@gmail.com>
2021-05-18 16:43:45 +0900

    Add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,22 @@ export default function Footer() {
   return (
     <Container>
       <Content>
-        ⓒ2021 ALL RIGHTS RESERVED
+        <a
+        href="https://github.com/yes-xodnd"
+        target="_blank"
+        rel="noopener noreferrer ">
+          <i aria-hidden className="fab fa-github"></i>
+          <span>yes-xodnd</span>
+        </a>
+        <div>ⓒ2021 ALL RIGHTS RESERVED</div>
       </Content>
     </Container>
   )
 }
 
 const Container = styled.footer`
-  padding: 1.5rem 2rem;
-  /* border-top: 1px dashed black; */
+  padding: 2rem;
+  border-top: 1px solid black;
   color: grey;
   text-align: center;
   font-size: ${({ theme }) => theme.font.sm };
@@ -21,4 +28,12 @@ const Container = styled.footer`
 const Content = styled.div`
   margin: auto;
   max-width: 1024px;
+  
+  i {
+    margin-right: 0.5rem;
+  }
+
+  div {
+    margin-top: 0.5rem;
+  }
 `
